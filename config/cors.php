2a11fe1b23caf5cdbd9b2794b2f414848692ae74
ishan <ishan@MacBook-Pro.local>
2026-08-28 23:47:56 +0530

    Allow Vercel frontend domain in CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://hr-frontend-lac.vercel.app'],
+
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
